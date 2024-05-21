61b1a81f9a7112276944af80454cc013178ed1a8
khi1601 <tgkim1601@gmail.com>
2024-05-21 23:34:44 +0900

    [Add]Simple Puzzle Merge

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,24 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-
-public class UIManager : MonoBehaviour
+using MyBox;
+public class UIManager : Singleton<UIManager>
 {
     public List<PanelModel> Panels;
-    private Queue<PanelInstanceModel> queue = new Queue<PanelInstanceModel>();
+    private List<PanelInstanceModel> listInstances = new List<PanelInstanceModel>();
 
     public void Show(string panelId)
     {
         PanelModel panelModel = Panels.FirstOrDefault(panel => panel.PanelId == panelId);
         Debug.Log("Show " + panelId);
 
-        if(panelModel != null)
+        if (panelModel != null)
         {
             Debug.Log(panelModel.PanelId);
-            
+
             var newInstancePanel = Instantiate(panelModel.PanelPrefab, transform);
 
-            queue.Enqueue(new PanelInstanceModel
+            listInstances.Add(new PanelInstanceModel
             {
                 PanelId = panelId,
                 PanelInstance = newInstancePanel
@@ -27,7 +27,7 @@ public class UIManager : MonoBehaviour
         }
         else
         {
-
+            Debug.LogWarning($"Trying to use panelId = {panelId}, but this is not found in Panels");
         }
     }
 }
